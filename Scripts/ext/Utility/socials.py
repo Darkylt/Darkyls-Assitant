d5f5ababcb72ef14e4e6649cc3831a51a60a65c5
Darkylt <darkprankingyt@gmail.com>
2024-12-15 16:36:02 +0100

    Improve socials

--- Scripts/ext/Utility/socials.py
+++ Scripts/ext/Utility/socials.py
@@ -24,6 +24,22 @@ import lightbulb
 
 plugin = lightbulb.Plugin("Socials", "A socials command")
 
+SOCIAL_LINKS = {
+    "Website": "https://darkylmusic.com",
+    "YouTube": "https://www.youtube.com/channel/UC1vqivmEx6wSMN3GGzdMM4A",
+    "Twitch": "https://www.twitch.tv/darkyltv",
+    "Spotify": "https://open.spotify.com/artist/3uSjSaWZrsNy7PK11dA74N?si=7W6Sx04IT6aHKPOfxq0X5g",
+    "Twitter/X": "https://twitter.com/DarkylMusic",
+    "TikTok": "https://www.tiktok.com/@darkyltv",
+    "SoundCloud": "https://soundcloud.com/darkylmusic",
+    "Apple Music": "https://music.apple.com/us/artist/dark-pranking/1544211920",
+    "Tidal": "https://tidal.com/browse/artist/35890590",
+    "Amazon Music": "https://music.amazon.de/artists/B0BNNQB85B/darkyl",
+    "Deezer": "https://www.deezer.com/de/artist/191835557",
+    "iHeart Radio": "https://www.iheart.com/artist/id-39272554/",
+    "Bluesky": "https://bsky.app/profile/darkylmusic.bsky.social",
+}
+
 
 @plugin.command
 @lightbulb.add_cooldown(3, 3, lightbulb.UserBucket)
@@ -56,26 +72,19 @@ async def socials_command(ctx: lightbulb.SlashContext):
             "dubstep",
         ]
 
-        text = "[Website](https://darkylmusic.com)\n[YouTube](https://www.youtube.com/channel/UC1vqivmEx6wSMN3GGzdMM4A)\n[Twitch](https://www.twitch.tv/darkyltv)\n[Spotify](https://open.spotify.com/artist/3uSjSaWZrsNy7PK11dA74N?si=7W6Sx04IT6aHKPOfxq0X5g)\n[Twitter/X](https://twitter.com/DarkylMusic)\n[TikTok](https://www.tiktok.com/@darkyltv)\n[SoundCloud](https://soundcloud.com/darkylmusic)\n[Apple Music](https://music.apple.com/us/artist/dark-pranking/1544211920)\n[Tidal](https://tidal.com/browse/artist/35890590)\n[Amazon Music](https://music.amazon.de/artists/B0BNNQB85B/darkyl)\n[Deezer](https://www.deezer.com/de/artist/191835557)\n[iHeart Radio](https://www.iheart.com/artist/id-39272554/)"
+        links_text = "\n".join(
+            [f"[{name}]({url})" for name, url in SOCIAL_LINKS.items()]
+        )
 
         embed = hikari.Embed(
             title="**Darkyl's Social Media Profiles:**",
-            description=f"Hi, I am Darkyl. Your gateway to a spectrum of {choice(random_text)} vibes straight outta Germany!\n\nLinks:\n{text}",
+            description=(
+                f"Hi, I am Darkyl. Your gateway to a spectrum of {choice(random_text)} vibes straight outta Germany!\n\n"
+                f"Links:\n{links_text}"
+            ),
             color=0x9900FF,
         )
         embed.set_thumbnail(thumbnail)
-        # embed.add_field(name="Website:", value="https://darkylmusic.com", inline=False)
-        # embed.add_field(name="YouTube:", value="https://www.youtube.com/channel/UC1vqivmEx6wSMN3GGzdMM4A", inline=False)
-        # embed.add_field(name="Twitch:", value="https://www.twitch.tv/darkyltv", inline=False)
-        # embed.add_field(name="Spotify:", value="https://open.spotify.com/artist/3uSjSaWZrsNy7PK11dA74N?si=7W6Sx04IT6aHKPOfxq0X5g", inline=False)
-        # embed.add_field(name="Twitter/X:", value="https://twitter.com/DarkylMusic", inline=False)
-        # embed.add_field(name="TikTok:", value="https://www.tiktok.com/@darkyltv", inline=False)
-        # embed.add_field(name="SoundCloud:", value="https://soundcloud.com/darkylmusic", inline=False)
-        # embed.add_field(name="Apple Music:", value="https://music.apple.com/us/artist/dark-pranking/1544211920", inline=False)
-        # embed.add_field(name="Tidal:", value="https://tidal.com/browse/artist/35890590", inline=False)
-        # embed.add_field(name="Amazon Music:", value="https://music.amazon.de/artists/B0BNNQB85B/darkyl", inline=False)
-        # embed.add_field(name="Deezer:", value="https://www.deezer.com/de/artist/191835557", inline=False)
-        # embed.add_field(name="iHeart Radio:", value="https://www.iheart.com/artist/id-39272554/", inline=False)
 
         await ctx.respond(embed=embed, flags=hikari.MessageFlag.EPHEMERAL)
     except Exception as e:
